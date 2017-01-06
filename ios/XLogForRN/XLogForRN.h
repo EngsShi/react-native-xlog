96cedfe61278165c7d05ba395f665afda1c8ae69
ShiHui <sh1@meitu.com>
2017-01-06 16:23:13 +0800

    初始化ios项目

---
+++ ios/XLogForRN/XLogForRN.h
@@ -0,0 +1,13 @@
+//
+//  XLogForRN.h
+//  XLogForRN
+//
+//  Created by shihui on 2017/1/6.
+//  Copyright © 2017年 EngsSH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLogForRN : NSObject
+
+@end
---
+++ ios/XLogForRN/XLogForRN.m
@@ -0,0 +1,13 @@
+//
+//  XLogForRN.m
+//  XLogForRN
+//
+//  Created by shihui on 2017/1/6.
+//  Copyright © 2017年 EngsSH. All rights reserved.
+//
+
+#import "XLogForRN.h"
+
+@implementation XLogForRN
+
+@end
