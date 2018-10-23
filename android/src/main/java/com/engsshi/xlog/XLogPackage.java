85f5b440f88374c9f379d5f5d2b4405d556f27f9
EngsShi <sh9701403@gmail.com>
2018-10-23 16:22:50 +0800

    RN0.47.0 remove override function createJSModules

--- android/src/main/java/com/engsshi/xlog/XLogPackage.java
+++ android/src/main/java/com/engsshi/xlog/XLogPackage.java
@@ -20,7 +20,6 @@ public class XLogPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new XLogModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
